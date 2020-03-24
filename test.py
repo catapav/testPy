448fed23ab3a214c2c6740bb376de75766b61e00
catapav <catapav.98@gmail.com>
2020-03-24 19:00:45 +0400

    Add name planet.

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
 import random
 
 number = random.randint(1, 8)
-print("Hello planet number ", number)
+
+
+def switch_demo(value):
+    switcher = {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus",
+        8: "Neptune",
+    }
+    print("Hello planet " + switcher.get(value))
+
+
+switch_demo(number)
